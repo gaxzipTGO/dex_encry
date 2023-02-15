65abbc578d39a0ed9fd8ef7cdec43c5177ebb594
gaxzipTGO <95045499+gaxzipTGO@users.noreply.github.com>
2023-02-16 01:52:26 +0800

    Update des.cpp

--- des.cpp
+++ des.cpp
@@ -12,45 +12,90 @@ int g_iptable[64] = { 58,50,42,34,26,18,10,2,
                       59,51,43,35,27,19,11,3,
                       61,53,45,37,29,21,13,5,
                       63,55,47,39,31,23,15,7 } ;
-
-vector<string> StringToBinary( string s ) {
-    string binaryString = "" ;
-    vector<string> block ;
-    int count = 0 ;
-    for ( auto ch : s ) {
-        binaryString += bitset<8>(ch).to_string()  ;
-        count += 1 ;
-        if ( count == 8 ) {
-            block.push_back(binaryString) ;
-            binaryString = "" ;
-            count = 0 ;
-        }
-    } // 每64bit 一個range
-    if ( count != 8 ) {
-        while( count != 8 ) {
-            binaryString += bitset<8>('\0').to_string()  ;  
-            count += 1 ;
-        }
-        block.push_back( binaryString ) ;
+string CharToString( char* ch_array, int size ) {
+    string str = "" ;
+    for ( int i = 0 ; i < size ; i ++ ) {
+        str += ch_array[i] ;
     }
-    return block ;
-} // 把明碼拆成許多區段
-
-string BinaryInitial(string str){
-    char changeChar[64] ;
-    for( int i = 0 ; i < 64 ; i ++ ) {
-        changeChar[g_iptable[i]-1] = str[i] ;
-    }
-    return string(changeChar) ;
+    return str ;
 }
+
+class Key{
+
+    private : string key ;
+
+    public : void LoadKey(string key) {
+        this->key = key ;
+    }
+
+} ;
+
+class Decoder{
+    public :
+    vector<string> StringToBinary( string s ) {
+        string binaryString = "" ;
+        vector<string> block ;
+        int count = 0 ;
+        for ( auto ch : s ) {
+            binaryString += bitset<8>(ch).to_string()  ;
+            count += 1 ;
+            if ( count == 8 ) {
+                block.push_back(binaryString) ;
+                binaryString = "" ;
+                count = 0 ;
+            }
+        } // 每64bit 一個range
+        if ( count < 8 && count != 0 ) {
+            while( count < 8 ) {
+                binaryString += bitset<8>('\0').to_string()  ;  
+                count += 1 ;
+            }
+            block.push_back( binaryString ) ;
+        }
+        return block ;
+    } // 把明碼拆成許多區段
+
+    string BinaryInitial(string str){
+        char changeChar[64] ;
+        for( int i = 0 ; i < 64 ; i ++ ) {
+            changeChar[g_iptable[i]-1] = str[i] ;
+        }
+        
+        return CharToString(changeChar,sizeof(changeChar)) ;
+    }
+
+    string GetLeftBit( string total_bit ) {
+        string str = "" ;
+        int size = total_bit.size() ;
+        for ( int i = 0  ; i < size/2 ; i ++ ) {
+            str += total_bit[i] ;
+        }
+        return str ;
+    }
+
+
+    string GetRightBit( string total_bit ) {
+        string str = "" ;
+        int size = total_bit.size() ;
+        for ( int i = size/2  ; i < size ; i ++ ) {
+            str += total_bit[i] ;
+        }
+        return str ;
+    }
+
+    
+
+} des_decoder ;
 
 
 int main() {
-    string inputData ="" ;
-    cin >> inputData ;
-    vector<string> binaryString = StringToBinary( inputData ) ; // 得到所有區段 接下來就可以做事了
+    string inputData ="kevinasdd" ;
+    //cin >> inputData ;
+    vector<string> binaryString = des_decoder.StringToBinary( inputData ) ; // 得到所有區段 接下來就可以做事了
     for ( auto str : binaryString ) {
-        cout << str << endl  ;
+        string temp = des_decoder.BinaryInitial(str) ;
+        cout << des_decoder.GetLeftBit(temp) << endl  ;
+        cout << des_decoder.GetRightBit(temp) << endl  ;
     }
 
 }
